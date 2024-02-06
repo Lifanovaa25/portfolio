1f9acd920e66ee18188921a7488478e9c5cf7940
Anna Lifanova <pashintsevaa@gmail.com>
2024-02-06 11:12:37 +0300

    кубик

--- src/components/HeadBlock/HeadBlock.tsx
+++ src/components/HeadBlock/HeadBlock.tsx
@@ -1,66 +1,27 @@
 import s from "./HeadBlock.module.scss";
-// import arrow from "./../../assets/images/arrow-double-start-svgrepo-com.svg";
 
 export const HeadBlock = () => {
-  
-  // const Items = [
-  //   {
-  //     skill: "JavaScript",
-  //   },
-  //   {
-  //     skill: "HTML 5",
-  //   },
-  //   {
-  //     skill: "CSS / SASS",
-  //   },
-  //   {
-  //     skill: "ReactJS",
-  //   },
-  //   {
-  //     skill: "Redux",
-  //   },
-  //   {
-  //     skill: "AntDesing",
-  //   },
-  //   {
-  //     skill: "Git",
-  //   },
-  // ];
   return (
     <>
       <section className={s.header_section}>
-        {/* <div className={height ? s.max_height : s.min_height}> */}
         <div className="container bg  ">
           <div className={s.wrapper}>
             <div className={s.left}>
               <h1 className={s.title}>
                 <span>Front-end </span> Developer
               </h1>
-
-              {/* <div className={s.skills}>
-                {Items.map((item) => (
-                  <span className={s.word}>
-                    <span className={s.letter}>{item.skill}</span>
-                  </span>
-                ))}
-              </div> */}
             </div>
-          </div>
-          {/* </div> */}
-          {/* {height && (
-            <div className={s.bottom_btn}>
-              <div
-                className={[s.btn_arrow, s.puls].join(" ")}
-                onClick={() => {
-                  setHeight(!height);
-                }}
-              >
-                <div className={s.bounce}>
-                  <img src={arrow} className={s.arrow} alt="" />
-                </div>
+            <div className={s.stage}>
+              <div className={s.cubespinner}>
+                <div className={s.face1}>JavaScript</div>
+                <div className={s.face2}>HTML 5</div>
+                <div className={s.face3}>CSS / SASS</div>
+                <div className={s.face4}>ReactJS</div>
+                <div className={s.face5}>Redux</div>
+                <div className={s.face6}>Git</div>
               </div>
             </div>
-          )} */}
+          </div>
         </div>
       </section>
     </>
